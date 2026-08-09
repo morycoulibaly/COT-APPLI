dae8b28ea56f12b7c1fdbc94070fa3f22979e3bb
Le Papillon <mory.coulibaly@epitech.eu>
2026-08-09 22:43:25 +0200

    ...

--- cotappli/backend/src/main.ts
+++ cotappli/backend/src/main.ts
@@ -14,10 +14,7 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
-    credentials: true,
-  });
+  app.enableCors({credentials: true});
 
   const port = process.env.PORT ?? 5000;
   await app.listen(port);
